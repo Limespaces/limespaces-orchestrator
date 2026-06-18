a62f93375ccd0dd853f40c76ea50dee96f53d9ab
Martin Petr <martinpetrnp@gmail.com>
2026-06-18 18:46:03 +0200

    Create nestjs project

---
+++ src/config.ts
@@ -0,0 +1,3 @@
+export const OrchestratorConfig = {
+  corsAllowedOrigins: [process.env.DASHBOARD_ORIGIN],
+};
---
+++ src/main.ts
@@ -0,0 +1,27 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe, VersioningType } from '@nestjs/common';
+import { OrchestratorConfig } from './config';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+    defaultVersion: '1',
+  });
+  app.setGlobalPrefix('api');
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  app.enableCors({
+    origin: OrchestratorConfig.corsAllowedOrigins,
+  });
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
